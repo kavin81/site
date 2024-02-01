46a3cf76a8d75bb171180fa5204cd95243bdfc49
kavin <kavin6462@gmail.com>
2024-02-01 13:20:20 +0530

    enabled prerender @ home page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,3 +1,5 @@
+export const prerender = true;
+
 import { getPartialPosts } from "$lib/server/Posts"
 export async function load({ params }) {
     return { body: await getPartialPosts(4)}
--- svelte.config.js
+++ svelte.config.js
@@ -8,5 +8,6 @@ const config = {
 	kit: {
 		adapter: adapter()
 	}
+	
 };
 export default config;
